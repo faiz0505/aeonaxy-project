b21d1f1197f372a69260cfceffd3c855dad8e25e
Faiz Al <faizali786313@gmail.com>
2024-04-13 11:07:33 +0530

    some problem fixes

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -1,19 +1,19 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import { getToken } from "../../utils";
-import { useEffect } from "react";
+// import { getToken } from "../../utils";
+// import { useEffect } from "react";
 
 export default function AuthLayout() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const checkAuthAndRedirect = async () => {
-      const session = await getToken();
-      if (!session) {
-        navigate("/sign-up");
-      }
-    };
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   const checkAuthAndRedirect = async () => {
+  //     const session = await getToken();
+  //     if (!session) {
+  //       navigate("/sign-up");
+  //     }
+  //   };
 
-    checkAuthAndRedirect();
-  }, []);
+  //   checkAuthAndRedirect();
+  // }, []);
 
   return (
     <main>
