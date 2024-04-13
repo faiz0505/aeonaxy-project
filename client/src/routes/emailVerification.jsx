aa6a2fb370e9a34c7fac0e30f7d4f671ac54e447
Faiz Al <faizali786313@gmail.com>
2024-04-13 10:53:33 +0530

    some problem fixes

--- client/src/routes/emailVerification.jsx
+++ client/src/routes/emailVerification.jsx
@@ -3,30 +3,30 @@ import { useSearchParams, useNavigate, useRevalidator } from "react-router-dom";
 import { api, apiUrl } from "../../utils";
 import toast from "react-hot-toast";
 const emailVerification = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
   const [searchParams] = useSearchParams();
-  const revalidator = useRevalidator();
-  const navigate = useNavigate();
+  // const revalidator = useRevalidator();
+  // const navigate = useNavigate();
   const token = searchParams.get("token");
-  useEffect(() => {
-    setIsLoading(true);
-    api
-      .post(`${apiUrl}/verify-token`, { token })
-      .then((res) => {
-        if (res.status === 200) {
-          toast.success("email verified successfully");
-          revalidator.revalidate();
-          setIsLoading(false);
-          navigate("/");
-        }
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        toast.error(err.message || `verification failed with ${err.status}`);
-      });
-  }, []);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   api
+  //     .post(`${apiUrl}/verify-token`, { token })
+  //     .then((res) => {
+  //       if (res.status === 200) {
+  //         toast.success("email verified successfully");
+  //         revalidator.revalidate();
+  //         setIsLoading(false);
+  //         navigate("/");
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       setIsLoading(false);
+  //       toast.error(err.message || `verification failed with ${err.status}`);
+  //     });
+  // }, []);
 
-  return <main>{isLoading && "verifying..."}</main>;
+  return <main>verifying... {token && JSON.stringify(token)}</main>;
 };
 
 export default emailVerification;
