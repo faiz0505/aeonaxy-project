b070111d1bbfbf09aa1b196e9cb549b7620c82df
Faiz Al <faizali786313@gmail.com>
2024-04-13 12:05:41 +0530

    vercel.json updated

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -13,12 +13,11 @@ const index = () => {
 
   if (!session) {
     return (
-      <EmailVerify session={session} />
-      // <main className="w-full py-60 flex justify-center items-center">
-      //   <h4 className="text-4xl font-bold">
-      //     Please Register/SingIn to continue...
-      //   </h4>
-      // </main>
+      <main className="w-full py-60 flex justify-center items-center">
+        <h4 className="text-4xl font-bold">
+          Please Register/SingIn to continue...
+        </h4>
+      </main>
     );
   }
 
